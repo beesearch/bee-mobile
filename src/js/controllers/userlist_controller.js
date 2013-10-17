19dbe92383e5bbe6a9f8391256e8f67769e04e0c
Alexandre Jarnoux <alexandre.jarnoux@beesearch.fr>
2013-10-17 15:53:39 +0200

    Fixed manually ddl & menu behavior, there is no animation though...

--- src/js/controllers/userlist_controller.js
+++ src/js/controllers/userlist_controller.js
@@ -1,8 +1,19 @@
 angular.module(_CONTROLLERS_).controller('UserListController', function($scope, $http) {  
     console.log('### UserListControler in');
+    
+    // init values
+    $scope.isDropdownOpen = false;
+    $scope.orderProp = 'lastName';
+
+    $scope.orderBy = function (prop) {
+		console.log('prop: ' + prop);
+		$scope.orderProp = prop;
+		$scope.isDropdownOpen = false;
+    };
+
     $http.get('http://zenbox-demo.herokuapp.com/zenbox-demo/rest/users/list').success(function(data) {
         $scope.users = data;
     });
-    $scope.orderProp = 'lastName';
+
     console.log('### UserListControler out');
 });
