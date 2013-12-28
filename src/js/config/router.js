5127a63af16f1d55e749f2fccea5934c15f152df
robbee <alexandre.bonte@beesearch.fr>
2013-12-28 10:09:39 +0100

    add product detail

--- src/js/config/router.js
+++ src/js/config/router.js
@@ -10,14 +10,42 @@ angular.module(_APP_).config([
 
     // Define routes here.
     $routeProvider
-        .when('/', { templateUrl: 'html/partials/home/index.html', controller: 'HomeController' })
-        .when('/login', { templateUrl: 'html/partials/home/login.html', controller: 'HomeLoginController' })
-        .when('/users', { templateUrl: 'html/partials/users/list.html', controller: 'UserListController' })
-        .when('/users/:userId', {templateUrl: 'html/partials/users/detail.html', controller: 'UserDetailController'})
-        .when('/products', { templateUrl: 'html/partials/products/list.html', controller: 'ProductListController' })
-        .when('/contacts', { templateUrl: 'html/partials/contacts/list.html', controller: 'ContactListController' })
-        .when('/notifications', {templateUrl: 'html/partials/phonegap/notifications.html', controller: 'NotificationsController'})
-        .when('/device', {templateUrl: 'html/partials/phonegap/device.html', controller: 'DeviceController'})
+        .when('/', { 
+          templateUrl: 'html/partials/home/index.html', 
+          controller: 'HomeController' 
+        })
+        .when('/login', { 
+          templateUrl: 'html/partials/home/login.html', 
+          controller: 'HomeLoginController' 
+        })
+        .when('/users', { 
+          templateUrl: 'html/partials/users/list.html', 
+          controller: 'UserListController' 
+        })
+        .when('/users/:userId', {
+          templateUrl: 'html/partials/users/detail.html', 
+          controller: 'UserDetailController'
+        })
+        .when('/products', { 
+          templateUrl: 'html/partials/products/list.html', 
+          controller: 'ProductListController' 
+        })
+        .when('/products/:productId', {
+          templateUrl: 'html/partials/products/detail.html', 
+          controller: 'ProductDetailController'
+        })
+        .when('/contacts', { 
+          templateUrl: 'html/partials/contacts/list.html', 
+          controller: 'ContactListController' 
+        })
+        .when('/notifications', {
+          templateUrl: 'html/partials/phonegap/notifications.html', 
+          controller: 'NotificationsController'
+        })
+        .when('/device', {
+          templateUrl: 'html/partials/phonegap/device.html', 
+          controller: 'DeviceController'
+        })
         .otherwise({ redirectTo: '/' });
   }
 ]);
