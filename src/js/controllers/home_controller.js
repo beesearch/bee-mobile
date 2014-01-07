0c60157a54c0d7bdb69389a038f49418859ccdab
Alexandre JARNOUX <alexandre.jarnoux@beesearch.fr>
2014-01-07 17:09:34 +0100

    Remember me mechanism implementation

--- src/js/controllers/home_controller.js
+++ src/js/controllers/home_controller.js
@@ -5,8 +5,22 @@ angular.module(_CONTROLLERS_).controller('HomeController', function($scope) {
 angular.module(_CONTROLLERS_).controller('HomeLoginController', function($scope, oauth2Token) {
     console.log('### HomeLoginController in');
     
-    $scope.signin = function() {
+    $scope.username = localStorage.username;
+    $scope.remembermechecked = (localStorage.username !== '');
+
+    $scope.signin_click = function() {
 		oauth2Token.retrieveToken($scope.username, $scope.password);
+	};
+
+	$scope.rememberme_change = function() {
+		if ($scope.rememberme == true)
+		{
+			localStorage.username = $scope.username;	
+		}
+		else
+		{
+			localStorage.username = '';
+		};
 	};
 
     console.log('### HomeLoginController out');
