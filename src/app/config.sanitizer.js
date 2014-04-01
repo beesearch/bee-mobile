33fd744bbb0a12ff9e8167a80de1f8c7360247d3
Alexandre JARNOUX <alexandre.jarnoux@beesearch.fr>
2014-04-01 16:34:04 +0200

    Removed useless injection annotations

--- src/app/config.sanitizer.js
+++ src/app/config.sanitizer.js
@@ -4,12 +4,8 @@
  * Defines the regex for link sanitation.
  *
  */
-angular.module(_APP_).config([
-  '$compileProvider',
-  function($compileProvider) {
-
-    // sanitize white list for angular/phonegap
-    var sanitation = new RegExp('^\s*(https?|ftp|mailto|file|tel|comgooglemaps|sms):');
-    $compileProvider.aHrefSanitizationWhitelist(sanitation);
-  }
-]);
+angular.module(_APP_).config(function($compileProvider) {
+	// sanitize white list for angular/phonegap
+	var sanitation = new RegExp('^\s*(https?|ftp|mailto|file|tel|comgooglemaps|sms):');
+	$compileProvider.aHrefSanitizationWhitelist(sanitation);
+});
