e78b1b061e85bfe9f5a0cd1c5790251c8b5cec78
Alexandre JARNOUX <alexandre.jarnoux@beesearch.fr>
2014-04-23 16:22:27 +0200

    Polishing side menus

--- src/index.tmpl.js
+++ src/index.tmpl.js
@@ -1,5 +1,10 @@
-angular.module(_CONTROLLERS_).controller('index', function($scope, $ionicModal, oauth2Token, oauth2Caller) {
+angular.module(_CONTROLLERS_).controller('index', function($scope, $ionicModal, $ionicSideMenuDelegate, $location, oauth2Token, oauth2Caller) {
 	console.log('### index controller in');
+
+	// Search action
+	$scope.search = function() {
+
+	}
 
 	// Create and load the login modal
 	$ionicModal.fromTemplateUrl('views/app/app-login.html', function(modal) {
@@ -10,16 +15,19 @@ angular.module(_CONTROLLERS_).controller('index', function($scope, $ionicModal, 
 		focusFirstInput: true
 	});
 
+	// Event action used when login is required from server
 	$scope.$on('event:auth-loginRequired', function(e) {
 		console.log('### index controller : Received event:auth-loginRequired event.');
 		openLoginModal();
 	});
 
+	// Event action used when login is confirmed from server
 	$scope.$on('event:auth-loginConfirmed', function(e) {
 		console.log('### index controller : Received event:auth-loginConfirmed event.');
 		closeLoginModal();
 	});
 
+	// Event action used when an error is received from server
 	$scope.$on('event:auth-errorReceived', function(e, error) {
 		console.log('### index controller : Received event:auth-errorReceived event. (error: ' + JSON.stringify(error) + ')');
 		$scope.message = error.error_description;
@@ -42,10 +50,20 @@ angular.module(_CONTROLLERS_).controller('index', function($scope, $ionicModal, 
 		oauth2Token.logout();
 	}
 
-	//Be sure to cleanup the modal by removing it from the DOM
+	// Cleanup the modal by removing it from the DOM
 	$scope.$on('$destroy', function() {
 		$scope.loginModal.remove();
 	});
+
+	$scope.toggleLeftSideMenu = function() {
+		$ionicSideMenuDelegate.toggleLeft();
+	};
+
+	$scope.goTo = function(page) {
+		console.log('Going to ' + page);
+		$ionicSideMenuDelegate.toggleLeft();
+		$location.url('/' + page);
+	};
 
 	console.log('### index controller out');
 });
