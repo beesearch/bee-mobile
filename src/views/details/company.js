52357521b93f51a44076b24cf5e2b0e6dc52eec5
robbee <alexandre.bonte@beesearch.fr>
2014-06-30 17:03:33 +0200

    change look&feel

--- src/views/details/company.js
+++ src/views/details/company.js
@@ -5,9 +5,27 @@ angular.module(_CONTROLLERS_).controller('company', function($scope, $stateParam
 
 	$scope.top5ProductsChart = {
 		series: [],
-		options: { chart: { type: 'bar'	} },
+		options: { 
+			chart: { 
+				type: 'bar'	
+			}
+		},
 		loading: true
 	}
+
+	// Make monochrome colors and set them as default for all pies
+	Highcharts.getOptions().plotOptions.pie.colors = (function () {
+	    var colors = [],
+	        base = Highcharts.getOptions().colors[0],
+	        i
+
+	    for (i = 0; i < 10; i++) {
+	        // Start out with a darkened base color (negative brighten), and end
+	        // up with a much brighter color
+	        colors.push(Highcharts.Color(base).brighten((i - 3) / 7).get());
+	    }
+	    return colors;
+	}());
 
 	beeModel.get({type: $stateParams.type, index: $stateParams.index, id: $stateParams.companyId}, function(data) {
 		// Set customer datas in scope
