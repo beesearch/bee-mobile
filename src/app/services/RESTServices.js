13d431d2eebe550ece777362aba672dcacbfaf35
robbee <alexandre.bonte@beesearch.fr>
2014-10-01 21:47:19 +0200

    add customer to mongo

--- src/app/services/RESTServices.js
+++ src/app/services/RESTServices.js
@@ -6,7 +6,7 @@ angular.module(_SERVICES_).factory('beeSearch', function($resource, oauth2Token,
 });
 
 angular.module(_SERVICES_).factory('beeModel', function($resource, oauth2Token, BACKEND_PROTOCOL, BACKEND_HOST, BACKEND_PORT) {
-	resource = $resource(BACKEND_PROTOCOL + '://' + BACKEND_HOST + ':' + BACKEND_PORT + '/db/abo/collection/customer/id/:id');
+	resource = $resource(BACKEND_PROTOCOL + '://' + BACKEND_HOST + ':' + BACKEND_PORT + '/db/:index/collection/:type/id/:id');
     //resource = $resource(BACKEND_PROTOCOL + '://' + BACKEND_HOST + ':' + BACKEND_PORT + '/db/:type/collection/:index/id/:id');
 
 	return oauth2Token.wrapActions(resource, ["get"]);
